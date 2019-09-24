83bdcfc7d3f8df7b2d8ecd8b33d4f9139a2f6bae
Ben Reynolds-Carr <bjr.kitz@gmail.com>
2019-09-24 14:10:23 +0100

    Installed enzyme and implemented setupTests

--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
-import expectExport from 'expect';
+// import expectExport from 'expect';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('First React component test with Enzyme', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
 });
 
-describe('examiming the syntax of Jest tests', () => {
-  it('sums numbers', () => {
-    expect(1 + 2).toEqual(3);
-    expect(2 + 2).toEqual(4)
-  })
-})
+
